5f34d1f15db0f1ef3e0c235ea781d9c17898a678
IamAmeen <alameenahmad68@gmail.com>
2022-09-11 07:07:12 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program to print all numbers zero to ten
+ *
+ * Return: Always (0) positive
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n')
+	return (0);
+}
