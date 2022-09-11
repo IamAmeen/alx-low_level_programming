2e103c02a800d20a4a4dbb7b7ec5979d9887faa4
IamAmeen <alameenahmad68@gmail.com>
2022-09-11 06:33:56 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program to print alphabets in lower and upper cases
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+	for (i = 0; i < 52; i++);
+	{
+		putchar(i);
+	}
+	
+	putchar('\n' );
+	return (0);
+}
