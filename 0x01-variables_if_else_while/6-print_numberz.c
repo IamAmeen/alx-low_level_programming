0ef06b0e17c55c83324d14d00a80791ca485ce7d
IamAmeen <alameenahmad68@gmail.com>
2022-09-11 12:29:35 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print numbers from (10) to (0) using putchar
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i > 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
