4d204a060b8a2c920c231867b0e7e5ce972f3fbe
IamAmeen <alameenahmad68@gmail.com>
2022-09-15 01:40:53 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case
+ * 
+ * Return: (0) success
+ */
+int _islower(int i);
+{
+	if (i >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
