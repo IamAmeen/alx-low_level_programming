7428c492e51ec90cba82e713377f1acd73ee1993
IamAmeen <alameenahmad68@gmail.com>
2022-09-16 07:00:06 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b; b > c;)
 	{
-		printf(a);
+		return (a);
 	}
-	else if (b > a && a > c)
+	else if (b > a; a > c;)
 	{
-		printf(b);
+		return (b);
 	}
 	else
 	{
-		printf(c);
+		return (c);
 	}
 	return (largest);
 }
