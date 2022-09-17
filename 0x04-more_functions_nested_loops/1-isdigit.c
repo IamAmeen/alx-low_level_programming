8df947733574397f90a163c828e70f46f557e66a
IamAmeen <alameenahmad68@gmail.com>
2022-09-17 06:01:25 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - check if c is a digit
- * return: 1 or 0
+ * return: 1  if c is digit or 0 if letter
  * @c: character
  */
 
 int _isdigit(int c)
 {
-	for (c >= 0 && c <= 9)
+	if (c >= '1' && c <= '9')
 	{
 		return (1);
 	}
