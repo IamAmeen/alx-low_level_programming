4712c59618b5a7280fa30fb1e33bda4d8504bbc0
IamAmeen <alameenahmad68@gmail.com>
2022-09-19 12:03:42 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen : returns the lenght of a string
- * @s: integer
- * return: count of string
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
 
 int _strlen(char *s)
